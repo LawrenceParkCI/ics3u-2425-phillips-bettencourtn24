bc35c95027a933629d341bdd5311794dd77a33b2
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-09-20 11:42:13 -0400

    This is a message about me.

---
+++ ICS3U-Phillips-BettencourtN/src/unit1/AboutMe.java
@@ -0,0 +1,20 @@
+package unit1;
+
+public class AboutMe {
+/**
+ * Description: This program describes who I am. It includes: My name, my grade, My teacher and what school I attend 
+ * Date: September 20. 2024
+ * @author Noa Phillips-Bettencourt
+ */
+	public static void main(String[] args) {
+		// This prints hello to the system console
+		System.out.println("About Me");
+		System.out.println("---------");
+		System.out.println("My name is Noa Phillips-Bettencourt");
+		System.out.println("I am in grade 11");
+		System.out.println("My teacher is Ms.Kemp");
+		System.out.println("I attend Lawrence Park Collegiate Institute");
+		System.out.println("I play soccer");
+		System.out.println("I have four pets, two dogs and two cats");
+		}
+}
