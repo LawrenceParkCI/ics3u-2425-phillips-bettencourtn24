6abecea5546487da8557d2009634b5d296fff85b
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-09-26 11:33:41 -0400

    Wrote my name and made an image.

---
+++ ICS3U-Phillips-BettencourtN/src/unit1/Asciinameart.java
@@ -0,0 +1,42 @@
+package unit1;
+
+public class Asciinameart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("\t __ _   __    __     ____  ____  ____  ____  ____  __ _   ___  __   _  _  ____  ____       ");
+	System.out.println("\t(  ( \\ /  \\  / _\\   (  _ \\(  __)(_  _)(_  _)(  __)(  ( \\ / __)/  \\ / )( \\(  _ \\(_  _) ");
+	System.out.println("\t/    /(  O )/    \\   ) _ ( ) _)   )(    )(   ) _) /    /( (__(  O )) \\/ ( )   /  )(   ");
+	System.out.println("\t\\_)__) \\__/ \\_/\\_/  (____/(____) (__)  (__) (____)\\_)__) \\___)\\__/ \\____/(__\\_) (__) ");
+	System.out.println("");
+	System.out.format("%50s","########..........,#######. ");
+	System.out.println("");
+	System.out.format("%53s", "###///,,,,///##,...##**********/## ");
+	System.out.println("");
+	System.out.format("%54s", "#////,,,,,,,,,**####***************##");
+	System.out.println("");
+	System.out.format("%54s","#///////////,,,,**##*****************#");
+	System.out.println("");
+	System.out.format("%55s","#/////////***,,**********************##");
+	System.out.println("");
+	System.out.format("%56s","#(///////*****************************#*");
+	System.out.println("");
+	System.out.format("%54s", "#//////***##**##*****#**(#*********##");
+	System.out.println("");
+	System.out.format("%53s","##////***************************#/");
+	System.out.println("");
+	System.out.format("%52s","##///**.  *****##*****  .*****##(");
+	System.out.println("");
+	System.out.format("%49s","##//**********************##");
+	System.out.println("");
+	System.out.format("%47s","/##*******************##");
+	System.out.println("");
+	System.out.format("%44s","##**************##");
+	System.out.println("");
+	System.out.format("%41s","##********##");
+	System.out.println("");
+	System.out.format("%38s","###*##");
+	}
+
+}
