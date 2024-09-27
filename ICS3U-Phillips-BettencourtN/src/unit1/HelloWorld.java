3388297fcd6004d83a7cbaf3d010043a56ffcb4d
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-09-27 10:09:48 -0400

    Added a comment.

--- ICS3U-Phillips-BettencourtN/src/unit1/HelloWorld.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/HelloWorld.java
@@ -5,7 +5,10 @@ package unit1;
  * @author Noa Phillips-Bettencourt 
  */
 public class HelloWorld {
-
+/**
+ * Entry point to the program.
+ * @param args unused 
+ */
 	public static void main(String[] args) {
 		// This prints hello to the system console
 		System.out.println("Hello World!!");
