7b3bb01353f6a58d5db85dab1b48e7596473bc5b
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-10-07 09:45:08 -0400

    completed

--- ICS3U-Phillips-BettencourtN/src/unit1/PlayingWithStrings.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/PlayingWithStrings.java
@@ -4,9 +4,7 @@ public class PlayingWithStrings {
 
 	public static void main(String[] args) {
 	
-String name1 = "Pierre Elliott Trudeau";
-String name2 = name1.substring(0, 6) + name1.substring(15);
-System.out.println(name2);
+ 
 
 
 
