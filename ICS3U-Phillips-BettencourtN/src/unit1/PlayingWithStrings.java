10e595e4453757112c3952e5a4062d04bfe46566
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-10-07 09:44:51 -0400

    completed assignment

--- ICS3U-Phillips-BettencourtN/src/unit1/PlayingWithStrings.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/PlayingWithStrings.java
@@ -4,7 +4,11 @@ public class PlayingWithStrings {
 
 	public static void main(String[] args) {
 	
-System.out.println("hello".substring(6));
+String name1 = "Pierre Elliott Trudeau";
+String name2 = name1.substring(0, 6) + name1.substring(15);
+System.out.println(name2);
+
+
 
 
 	}
