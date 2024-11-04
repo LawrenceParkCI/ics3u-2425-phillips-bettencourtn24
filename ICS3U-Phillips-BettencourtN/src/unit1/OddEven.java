ec44ad25fd0eacbcee0471e4b8c7560b23a1baf9
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-11-04 11:14:19 -0500

    Edited program. deleted unecessary lines.

--- ICS3U-Phillips-BettencourtN/src/unit1/OddEven.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/OddEven.java
@@ -7,35 +7,21 @@ import java.util.Scanner;
  * @author Noa Phillips-Bettencourt 
  */
 public class OddEven {
-	private int number; 
+	/**
+	 * Entry point to the program
+	 * @param args unused 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner (System.in);
+		System.out.print("Enter an integer: ");
+		int number = scanner. nextInt();
 
-	public OddEven (int number) {
-		this.number = number;
-	}
-
-	public void checkOddEven() {
 		if (number % 2==0) {
 			System.out.println(number + " is an even number.");
 		} else { 
 			System.out.println(number + " is an odd number.");
 		}
-	}
-
-
-	/**
-	 * Entry point to the program
-	 * @param args unused 
-	 */
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner (System.in);
-		System.out.print("Enter an integer: ");
-		int num = scanner. nextInt();
-
-		OddEven oddevenChecker = new OddEven(num);
-		oddevenChecker.checkOddEven();
-		scanner.close();
 
 	}
 
--- ICS3U-Phillips-BettencourtN/src/unit1/PositiveNegative.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/PositiveNegative.java
@@ -16,12 +16,12 @@ public class PositiveNegative {
 	 * @param args unused 
 	 */
 	public static void main(String[] args) {
-	
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter number:");
-		
+
 		int number = sc.nextInt();
-	
+
 
 		if (number >= 0) {
 			System.out.println(number + " is a positive number.");
