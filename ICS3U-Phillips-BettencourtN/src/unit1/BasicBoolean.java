ea2d83e05041935088028757eceb7dc9ecbd67e4
342899739 <342899739@C-MXL2393F1G.tdsb.on.ca>
2024-11-05 09:50:20 -0500

    completed program

--- ICS3U-Phillips-BettencourtN/src/unit1/BasicBoolean.java
+++ ICS3U-Phillips-BettencourtN/src/unit1/BasicBoolean.java
@@ -28,24 +28,24 @@ public class BasicBoolean {
 		int secondNum = 10;
 		double thirdNum = 5.5;
 
-		//this prints out ___true ____ because ________________
+		//this prints out ___true ____ because ____the first number is five which is smaller than 10 ____________
 		System.out.println("1: " + (firstNum < secondNum)); 
 
-		//this prints out ________ because ________________
+		//this prints out __false______ because _____the first number is smaller than the second ___________
 		System.out.println("2: " + (firstNum > secondNum)); 
 
-		//this prints out ________ because ________________
+		//this prints out ___true_____ because ______the first number is even to the thrid but only because the third number was turned to an int rather than a double __________
 		System.out.println("3: " + (firstNum <= (int)thirdNum)); 
-		//this prints out ________ because ________________
+		//this prints out _____true___ because _______the first number is 5 and the third number is 5.5 meaning it is either considered smaller or even depending on if you change the third number to an int. _________
 		System.out.println("4: " + (firstNum <= thirdNum)); 
 
 		/*Test out these boolean operators. Figure out the meaning
-		 * > means:
-		 * < means:
-		 * <= means:
-		 * >= means:
-		 * == means:
-		 * != means:
+		 * > means: left side  is bigger than right side 
+		 * < means: right side is bigger than left side 
+		 * <= means: right side is either bigger or even to left side 
+		 * >= means: left side is bigger or even to the right side 
+		 * == means: both sides are equal 
+		 * != means: not equal to
 		 */
 
 	}
